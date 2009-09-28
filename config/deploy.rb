df076308c0a83f1f83255284349eabb66b3e87d8
Andrew Roth <andrewroth@gmail.com>
2009-09-28 14:32:35 -0400

    update with our deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "c4c_utility"
+set :repository,  "git://github.com/andrewroth/c4c_utility.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+role :app, "pat.powertochange.org"
+role :web, "pat.powertochange.org"
+role :db,  "pat.powertochange.org", :primary => true
+
+set :user, 'deploy'
+set :deploy_to, "/var/www/c4c_utility.campusforchrist.org"
