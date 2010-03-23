ab9a1be66750d7d9c09da7ac4850a962844a4829
Andrew Roth <andrewroth@gmail.com>
2010-03-23 15:08:56 -0400

    use c4c.dev always

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -6,7 +6,7 @@ set :repository,  "git://github.com/andrewroth/c4c_utility.git"
 set :scm, "git"
 set :keep_releases, 3
 set :rails_env, "development"
-set :branch, "multisite"
+set :branch, "c4c.dev"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
